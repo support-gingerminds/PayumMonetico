e26c4d828680381648b2be55640902a9678bc9b6
tlenoble <tlenoble@gingerminds.fr>
2024-06-19 17:04:12 +0200

    fix(Config)

--- src/Action/Api/PaymentResponseAction.php
+++ src/Action/Api/PaymentResponseAction.php
@@ -26,8 +26,6 @@ class PaymentResponseAction extends AbstractApiAction
 
         $this->gateway->execute($httpRequest = new GetHttpRequest());
 
-        dd($this, $request, $model, $httpRequest);
-
         if (isset($httpRequest->request['code-retour'])) {
             $data = $httpRequest->request;
         } elseif (isset($httpRequest->query['code-retour'])) {
