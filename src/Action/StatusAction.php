2c9c8fe30d8c447f8898bcd8fb5a0c640ba7b354
tlenoble <tlenoble@gingerminds.fr>
2024-06-24 10:57:51 +0200

    fix(Config)

--- src/Action/StatusAction.php
+++ src/Action/StatusAction.php
@@ -25,6 +25,8 @@ class StatusAction implements ActionInterface
 
         $model = ArrayObject::ensureArrayObject($request->getModel());
 
+        dd($model);
+
         if (false == $code = $model['code-retour']) {
             if (false != $code = $model['state_override']) {
                 if ($code === 'canceled') {
